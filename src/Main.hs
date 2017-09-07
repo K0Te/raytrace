1c03d2c0ccd7eb6661a31667c8f8f1d6545975f1
Oleg Nykolyn <juravel2@gmail.com>
2017-09-07 20:49:20 +0300

    Optimize using vector, remove unused code.

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,14 @@
 module Main where
 
+import Data.Vector (Vector, (!), fromList)
 import Data.Tuple (swap)
 import GHC.Word (Word8)
 import Codec.Picture
 import Codec.Picture.Types
 
 -- Where is camera pointed ?
-maxX = 480
-maxY = 240
+maxX = 640
+maxY = 480
 
 main :: IO ()
 main = do
@@ -15,7 +16,7 @@ main = do
                 --, Line (Point 0 100 0) (Vector 1 (-1) 1)
                 Plane]
   let myPlot = dumbRender maxX maxY objects
-  let rim = generateImage (\x y -> let (r, g, b) = myPlot !! (x+(maxY-y-1)*maxX) in PixelRGB8 r g b) maxX maxY
+  let rim = generateImage (\x y -> let (r, g, b) = myPlot ! (x+(maxY-y-1)*maxX) in PixelRGB8 r g b) maxX maxY
   -- \let rim = generateFoldImage  (\acc -> \x -> \y -> (acc+1, PixelRGB8 (acc `mod` 255) ( 255) (255))) 0 640 480
   writePng "test.png" rim
   putStrLn "hello world"
@@ -27,28 +28,14 @@ enumCoord xs = let coords = map toCoord (fst <$> zip [0..] xs)
                where toCoord num = swap $ num `divMod` maxX
 
 type MyPixel = (Word8, Word8, Word8)
+type MyPlot =  [MyPixel]
 class Object a where
-  draw :: a -> [MyPixel] -> [MyPixel]
+  draw :: a -> MyPlot -> MyPlot
 
 
 data Point = Point Int Int Int deriving Show
-data Vector = Vector Int Int Int deriving Show
-data Line = Line Point Vector deriving Show
-
-instance Object Line where
-  draw line plot =
-    snd . markWhite <$> enumCoord plot
-    where
-      markWhite :: ((Int, Int), MyPixel) -> ((Int, Int), MyPixel)
-      markWhite (screenPoint, pixel) = (screenPoint, newPixel)
-        where newPixel = if visibleLine screenPoint line then whitePixel else pixel
-              whitePixel = (200, 200, 200)
-              visibleLine :: (Int, Int) -> Line -> Bool
-              visibleLine (sx, sy) (Line (Point x y z) (Vector vx vy vz)) = isTooFar lineY lineZ sy
-                where deltaK = fromIntegral (sx - x) / fromIntegral vx
-                      lineY = deltaK * fromIntegral vy + fromIntegral y
-                      lineZ = deltaK * fromIntegral vz + fromIntegral z
-                      isTooFar ly lz sy = ((abs . sqrt) $ (fromIntegral sy - ly) ** 2 + lz ** 2) < 30
+data GVector = GVector Int Int Int deriving Show
+data Line = Line Point GVector deriving Show
 
 data Plane = Plane
 cameraPos = Point (maxX `div` 2) (maxY * 3 `div` 4) maxX
@@ -82,8 +69,8 @@ instance Object Plane where
               lineToPixel screenPoint _ = case screenPointToPlanePoint screenPoint of Nothing -> pixel
                                                                                       (Just plx) -> if isBlack plx then blackPixel else whitePixel
 
-type Render a = Int -> Int -> [a] -> [MyPixel]
+type Render a = Int -> Int -> [a] -> Vector MyPixel
 
 dumbRender :: Object a => Render a
-dumbRender x y = foldr draw emptyPlot
+dumbRender x y objects = fromList $ foldr draw emptyPlot objects
   where emptyPlot = replicate (x*y) (0, 0, 0)
